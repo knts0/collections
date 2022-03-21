9902b0c6f7e8096de5f8ed1095268a05f8191da0
Kana Otawara <shimmer22.0204@gmail.com>
2022-03-21 20:51:53 +0900

    create add_employee_to_department

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
 
@@ -11,6 +13,14 @@ fn main() {
 
     // string
     convert_strings_to_pig_latin("first apple");
+
+    // vector & hash map
+    let mut employee_map: HashMap<String, Vec<String>> = HashMap::new();
+    add_employee_to_department(&mut employee_map, "Engineering".to_string(), "Sally".to_string());
+    add_employee_to_department(&mut employee_map, "Sales".to_string(), "Amir".to_string());
+    add_employee_to_department(&mut employee_map, "Sales".to_string(), "Bob".to_string());
+
+    println!("{:?}", &employee_map);
 }
 
 fn calc_median(vector: &mut Vec<i32>) -> &i32 {
@@ -29,4 +39,12 @@ fn convert_strings_to_pig_latin(text: &str) -> () {
         result.push_str(format!(" {}", converted_word.as_str()).as_str());
     }
     println!("{}", result.trim());
+}
+
+fn add_employee_to_department(map: &mut HashMap<String, Vec<String>>, department: String, employee: String) -> &mut HashMap<String, Vec<String>>{
+    let employees: &mut Vec<String> = map.entry(department).or_insert(Vec::new());
+
+    employees.push(employee);
+
+    map
 }
